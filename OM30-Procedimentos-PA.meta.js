78a9d664ee13ba9159f5c95302ef8ce15eb43daf
pdrjsampaio <pdrjustinosampaio@gmail.com>
2026-09-20 19:56:43 -0300

    Remove unnecessary PA metadata file

--- OM30-Procedimentos-PA.meta.js
+++
@@ -1,7 +0,0 @@
-// ==UserScript==
-// @name         OM30 - Procedimentos PA
-// @namespace    https://om30.com.br/
-// @version      1.3.1
-// @updateURL    https://raw.githubusercontent.com/pdrjsampaio/om30-userscripts/main/OM30-Procedimentos-PA.meta.js
-// @downloadURL  https://raw.githubusercontent.com/pdrjsampaio/om30-userscripts/main/OM30-Procedimentos-PA.user.js
-// ==/UserScript==

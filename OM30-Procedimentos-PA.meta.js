bcd402339f350bd2d4320b2db23b8103c5f2964c
pdrjsampaio <pdrjustinosampaio@gmail.com>
2026-09-20 19:54:23 -0300

    Add PA Tampermonkey update metadata

---
+++ OM30-Procedimentos-PA.meta.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         OM30 - Procedimentos PA
+// @namespace    https://om30.com.br/
+// @version      1.3.1
+// @updateURL    https://raw.githubusercontent.com/pdrjsampaio/om30-userscripts/main/OM30-Procedimentos-PA.meta.js
+// @downloadURL  https://raw.githubusercontent.com/pdrjsampaio/om30-userscripts/main/OM30-Procedimentos-PA.user.js
+// ==/UserScript==

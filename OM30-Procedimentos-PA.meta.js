8510e9b618b5d02ee4e31d85f8c30affde25bc76
pdrjsampaio <pdrjustinosampaio@gmail.com>
2026-09-20 19:25:43 -0300

    Add Tampermonkey update metadata

---
+++ OM30-Procedimentos-PA.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         OM30 - Procedimentos PA
+// @namespace    https://om30.com.br/
+// @version      0.4.1
+// @description  Busca rápida de Exames, Procedimentos/CIDs e Medicamentos no Pronto Atendimento.
+// @updateURL    https://raw.githubusercontent.com/pdrjsampaio/om30-userscripts/main/OM30-Procedimentos-PA.meta.js
+// @downloadURL  https://raw.githubusercontent.com/pdrjsampaio/om30-userscripts/main/OM30-Procedimentos-PA.user.js
+// @author       Pedro Sampaio - Samp
+// @match        https://guaruja.saudesimples.net/prontuarios/*
+// @match        https://guarujahomolog.saudesimples.net/prontuarios/*
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
